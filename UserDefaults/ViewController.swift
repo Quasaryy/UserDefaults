13bbd39838258ffb061103b1a3a119edafe6c804
Yury <quasaryy@gmail.com>
2023-02-25 02:02:33 +0400

    Added logic to save text from textField

--- UserDefaults/ViewController.swift
+++ UserDefaults/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        textField.placeholder = "Enter any text"
+        view.backgroundColor = .systemGray6
+        button.setTitle("Save", for: .normal)
+        label.text = UserDefaults.standard.string(forKey: "textField")
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        UserDefaults.standard.set(textField.text, forKey: "textField")
+        label.text = UserDefaults.standard.value(forKey: "textField") as? String ?? ""
+    }
+    
 }
 
